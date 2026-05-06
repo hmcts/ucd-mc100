0ad00aa5abbd6ec8dce6b5898e3cbc6646aa9882
Katy de la Motte <katy.delamotte@justice.gov.uk>
2026-05-06 11:51:21 +0100

    /

--- app/routes.js
+++ app/routes.js
@@ -22,8 +22,12 @@ router.post('/income-sources', function (request, response) {
     incomeSources = [incomeSources]
   }
 
-  if (incomeSources && incomeSources.includes('Employment')) {
+  if (incomeSources && incomeSources.includes('Employment') && incomeSources.includes('Benefits')) {
+    response.redirect('/benefits')
+  } else if (incomeSources && incomeSources.includes('Employment')) {
     response.redirect('/employer-details')
+  } else if (incomeSources && incomeSources.includes('Benefits')) {
+    response.redirect('/benefits')
   } else {
     response.redirect('/spending-questions')
   }
@@ -43,6 +47,29 @@ router.post('/spending-questions', function (request, response) {
 
 })
 
+router.post('/any-benefits', function (request, response) {
+
+  const receiveBenefits = request.session.data['receiveBenefits']
+
+  if (receiveBenefits === 'yes') {
+    response.redirect('/benefits')
+  } else {
+    response.redirect('/employment-status')
+  }
+
+})
+
+router.post('/employment-status', function (request, response) {
+
+  const employmentStatus = request.session.data['employmentStatus']
+
+  if (employmentStatus === 'yes') {
+    response.redirect('/employer-details')
+  } else {
+    response.redirect('/income-sources')
+  }
+
+})
 
 
 // ====================== EXPORT ======================
